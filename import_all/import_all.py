0aa3a776af51804f6a4eb23cd4916df953ec1816
Birdming Lai <birdming22@hotmail.com>
2015-08-13 14:56:08 +0800

    add import_all

---
+++ import_all/import_all.py
@@ -0,0 +1,14 @@
+'''
+## Import all moudles in directory /moudles
+
+Run and result
+
+    $ python import_all.py
+    hello
+
+'''
+
+from modules import *
+
+foo.bar()
+
---
+++ import_all/modules/__init__.py
@@ -0,0 +1,7 @@
+import os
+import glob
+
+modules = glob.glob(os.path.dirname(__file__) + "/*.py")
+__all__ = [os.path.basename(m)[:-3] for m in modules]
+__all__.remove('__init__')
+
